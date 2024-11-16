ad9d6e7ca6fcbb2fa5f4295be7a4fc383423c234
Pol Resina <pol.resina.martinez@estudiantat.upc.edu>
2024-11-16 14:14:36 +0100

    improve data_merge

--- data_merge.py
+++ data_merge.py
@@ -11,7 +11,8 @@ WIDTH = 160
 def add_attribute_columns(products_df, attributes_df):
     # Get unique attribute names
     unique_attributes = attributes_df['attribute_name'].unique()
-    
+
+
     # Create a copy of the products dataframe
     result_df = products_df.copy()
     
@@ -20,12 +21,15 @@ def add_attribute_columns(products_df, attributes_df):
         # Create a mapping dictionary for this attribute
         attr_mapping = attributes_df[attributes_df['attribute_name'] == attr].set_index('cod_modelo_color')['des_value']
         # Add the column with the attribute values, filling missing values with empty string
-        result_df[attr] = result_df['cod_modelo_color'].map(attr_mapping).fillna("NAN")
+        result_df[attr] = result_df['cod_modelo_color'].map(attr_mapping).fillna("INVALID")
     return result_df
 
 
 # read csv
-products_df = pd.read_csv(CSV_PATH + 'product_data.csv') 
+products_df = pd.read_csv(CSV_PATH + 'product_data.csv')
+# remove column
+products_df.drop(columns= ["cod_color"], inplace=True)
+
 attributes_df = pd.read_csv(CSV_PATH + 'attribute_data.csv')
 result = add_attribute_columns(products_df, attributes_df)
 
